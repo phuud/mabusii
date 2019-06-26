c86064f9ce6f4cb9d6db61321f2a4e57a7fee11c
phuud <trunkb@gmail.com>
2019-06-26 10:59:45 +0800

    add gh-pages in /docs

---
+++ docs/precache-manifest.cf536a0e55afcdde5736b2f6ff72feba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a8929231fc28346dddb42c530eb59c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "920e26218f5be904fc0b",
+    "url": "/static/css/2.3f1c995d.chunk.css"
+  },
+  {
+    "revision": "ede1c72bdd06cd2ae8f1",
+    "url": "/static/css/main.c79c028f.chunk.css"
+  },
+  {
+    "revision": "920e26218f5be904fc0b",
+    "url": "/static/js/2.97c6a0af.chunk.js"
+  },
+  {
+    "revision": "ede1c72bdd06cd2ae8f1",
+    "url": "/static/js/main.f4307e0f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
